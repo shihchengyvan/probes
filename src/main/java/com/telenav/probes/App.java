08ba0f777131d4717663c55f452d7e8035c69377
jinhua <jhzhu@telenavsoftware.com>
2020-09-25 10:39:22 +0800

    clean code

--- src/main/java/com/telenav/probes/App.java
+++ src/main/java/com/telenav/probes/App.java
@@ -23,32 +23,9 @@ public class App {
         SparkSession ss = SparkSession.builder().appName("probes").master("local[*]").getOrCreate();
 
         // read raw data
-        Dataset<Row> lines = ss.read().json("src/main/resources/raw/not_null.json");
-        Dataset<Section> oneLineSections = lines.map(line -> {
-            Row payload = line.getStruct(line.fieldIndex(PAYLOAD));
-            Row logContext = payload.getStruct(payload.fieldIndex(LOG_CONTEXT));
-            String carId = logContext.getString(logContext.fieldIndex(CAR_ID));
-            if (StringUtils.isBlank(carId))
-                return null;
-            Section section = new Section();
-            section.setCarId(carId);
-            List<Row> probeList = payload.getList(payload.fieldIndex(PROBE_LIST));
-            for (Row probe : probeList) {
-                Instant instant = new Instant();
-                long timestamp = probe.getLong(probe.fieldIndex(TIMESTAMP));
-                if (timestamp <= TIMESTAMP_20200701) {
-                    continue;
-                } else {
-                    instant.setTimestamp(timestamp);
-                }
-                instant.setLat(probe.getDouble(probe.fieldIndex(LAT)));
-                instant.setLon(probe.getDouble(probe.fieldIndex(LON)));
-                section.getInstantList().add(instant);
-            }
-            if (section.getInstantList().isEmpty())
-                return null;
-            return section;
-        }, Encoders.bean(Section.class)).filter(Objects::nonNull);
+        Dataset<Row> lines = ss.read().json("src/main/resources/raw/not_null.txt");
+        Dataset<Section> oneLineSections = lines.map(App::transform2Section,
+                                        Encoders.bean(Section.class)).filter(Objects::nonNull);
 
         Dataset<Tuple2<String, Section>> oneCarSections = oneLineSections.groupByKey(Section::getCarId,
                 Encoders.STRING())
@@ -66,37 +43,7 @@ public class App {
                 sections.add(x._2);
                 return sections.iterator();
             }
-
-            Section tempSection = new Section();
-            tempSection.setCarId(carId);
-            tempSection.getInstantList().add(allInstants.get(0));
-            Instant lastEffectiveInstant = allInstants.get(0);
-            for (int i = 1; i < allInstants.size(); i++) {
-                Instant currentInstant = allInstants.get(i);
-                long interval = currentInstant.getTimestamp() - lastEffectiveInstant.getTimestamp();
-                // split a section if time interval is too long
-                if (interval > TIME_INTERVAL_THRESHOLD) {
-                    sections.add(tempSection);
-                    // initiate a new section
-                    tempSection = new Section();
-                    tempSection.setCarId(carId);
-                    tempSection.getInstantList().add(currentInstant);
-                    lastEffectiveInstant = currentInstant;
-                } else if (interval != 0) {
-                    // add or drop instants
-                    double distance = (new Coordinate(currentInstant.getLon(), currentInstant.getLat())
-                            .distance(new Coordinate(lastEffectiveInstant.getLon(), lastEffectiveInstant.getLat())))
-                            / 180 * Math.PI * EARTH_RADIUS;
-                    double intervalSpeed = distance / interval;
-                    if (intervalSpeed <= SPEED_THRESHOLD) {
-                        tempSection.getInstantList().add(currentInstant);
-                        lastEffectiveInstant = currentInstant;
-                    }
-                }
-            }
-            if (!tempSection.getInstantList().isEmpty()) {
-                sections.add(tempSection);
-            }
+            addCleanedSection(carId, allInstants, sections);
             return sections.iterator();
         }, Encoders.bean(Section.class));
 
@@ -106,6 +53,62 @@ public class App {
         geojsons.coalesce(1).write().mode(SaveMode.Overwrite).text("src/main/resources/output/");
 
 //        getDetailsJsonStringDataset(cleanedSections).coalesce(1).write().mode(SaveMode.Overwrite).text("src/main/resources/output_detail/");
+    }
+    private static void addCleanedSection(String carId, List<Instant> allInstants, List<Section> sections){
+
+        Section tempSection = new Section();
+        tempSection.setCarId(carId);
+        tempSection.getInstantList().add(allInstants.get(0));
+        Instant lastEffectiveInstant = allInstants.get(0);
+        for (int i = 1; i < allInstants.size(); i++) {
+            Instant currentInstant = allInstants.get(i);
+            long interval = currentInstant.getTimestamp() - lastEffectiveInstant.getTimestamp();
+            // split a section if time interval is too long
+            if (interval > TIME_INTERVAL_THRESHOLD) {
+                sections.add(tempSection);
+                // initiate a new section
+                tempSection = new Section();
+                tempSection.setCarId(carId);
+                tempSection.getInstantList().add(currentInstant);
+                lastEffectiveInstant = currentInstant;
+            } else if (interval != 0) {
+                // add or drop instants
+                double distance = (new Coordinate(currentInstant.getLon(), currentInstant.getLat())
+                        .distance(new Coordinate(lastEffectiveInstant.getLon(), lastEffectiveInstant.getLat())))
+                        / 180 * Math.PI * EARTH_RADIUS;
+                double intervalSpeed = distance / interval;
+                if (intervalSpeed <= SPEED_THRESHOLD) {
+                    tempSection.getInstantList().add(currentInstant);
+                    lastEffectiveInstant = currentInstant;
+                }
+            }
+        }
+        if (!tempSection.getInstantList().isEmpty()) {
+            sections.add(tempSection);
+        }
+    }
+    private static Section transform2Section(Row line){
+        Row payload = line.getStruct(line.fieldIndex(PAYLOAD));
+        Row logContext = payload.getStruct(payload.fieldIndex(LOG_CONTEXT));
+        String carId = logContext.getString(logContext.fieldIndex(CAR_ID));
+        if (StringUtils.isBlank(carId))
+            return null;
+        Section section = new Section();
+        section.setCarId(carId);
+        List<Row> probeList = payload.getList(payload.fieldIndex(PROBE_LIST));
+        for (Row probe : probeList) {
+            Instant instant = new Instant();
+            long timestamp = probe.getLong(probe.fieldIndex(TIMESTAMP));
+            if (timestamp <= TIMESTAMP_20200701) {
+                continue;
+            } else {
+                instant.setTimestamp(timestamp);
+            }
+            instant.setLat(probe.getDouble(probe.fieldIndex(LAT)));
+            instant.setLon(probe.getDouble(probe.fieldIndex(LON)));
+            section.getInstantList().add(instant);
+        }
+        return (section.getInstantList().isEmpty()) ? null : section;
     }
     // provide a wkt method for the geometry
     private static void generateGeometryWKT(Section section, StringBuilder sBuilder){
